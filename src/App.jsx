c0b3087ba9c17584d4a6c838590c0254421c0782
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-12 11:54:45 +0200

    npm i react-apexcharts. chart added..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-import "./App.scss";
+import Chart from "react-apexcharts";
+import { useState, useEffect } from "react";
 
-function App() {
-  return <div className="App">Hallo!</div>;
-}
+export const App = () => {
+  const [options, setOptions] = useState({});
+  const [series, setSeries] = useState([]);
 
-export default App;
+  useEffect(() => {
+    setOptions({
+      chart: {
+        id: "apexchart-example",
+      },
+      xaxis: {
+        categories: [
+          2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
+          2022,
+        ],
+      },
+    });
+    setSeries([
+      {
+        name: "series-1",
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125, 30, 45, 65],
+      },
+      {
+        name: "series-2",
+        data: [20, 30, 55, 75, 63, 60, 80, 134, 110, 40, 55, 64],
+      },
+    ]);
+  }, []);
+
+  return (
+    <>
+      <Chart
+        options={options}
+        series={series}
+        type="line" //"bar","line", "donut","area", "radar", "histogram", "pie", "radialBar", scatter, bubble, heatmap, candlestick
+        width={800}
+        height={520}
+      />
+    </>
+  );
+};
